9e451d9d71f02b7462bcc45068838511b912d9b9
Abinaya-251 <aabinayaantony251@gmail.com>
2024-11-27 16:48:02 +0000

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+// frontend/src/App.js
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    const [message, setMessage] = useState('Loading...');
+    const [backendUrl, setBackendUrl] = useState('');
+
+    useEffect(() => {
+        // Determine the environment
+        const isLocal = window.location.hostname === 'localhost';
+	    const host = window.location.hostname;
+
+        // Set backend URLs
+        const backends = isLocal
+            ? ['http://localhost:5001', 'http://localhost:5002'] // Local development URLs
+            : [`http://${host}:5001`, `http://${host}:5002`]; //  cloud IPs
+
+        // Pick a backend dynamically
+        const randomBackend = backends[Math.floor(Math.random() * backends.length)];
+        setBackendUrl(randomBackend);
+
+        // Fetch data from the selected backend
+        fetch(randomBackend)
+            .then((response) => response.text())
+            .then((data) => setMessage(data))
+            .catch((error) => setMessage('Error fetching message.'));
+    }, []);
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>{message}</h1>
+            <p>Fetched from: {backendUrl}</p>
+        </div>
+    );
+};
+
+export default App;
+
